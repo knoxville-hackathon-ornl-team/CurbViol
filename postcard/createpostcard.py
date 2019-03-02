bf960cbad13265519e6dfee5594fe6caff5842f1
Mark Coletti <colettima@ornl.gov>
2019-03-02 11:54:41 -0500

    Checkpoint commit. Breaking out responsibilities for LaTeX parts into separate functions.

--- postcard/createpostcard.py
+++ postcard/createpostcard.py
@@ -1,11 +1,17 @@
 #!/usr/bin/env python3
 """
-    Used to create LaTeX output for a postcard.
+    Used to create LaTeX output for a postcard.  This prints two pages for a 3.5" x 5" postcard.
 """
+import sys
 import argparse
+import csv
 from pathlib import Path
+from pprint import pprint
 
-postcard_tex = """\\documentclass{article}
+# This is the script return code if we cannot open the curbside violations CSV file
+NO_CSV_FILE_ERROR = 1
+
+postcard_tex_preamble = """\\documentclass{article}
 
 \\pagestyle{empty}
 \\setlength\\parindent{0pt}
@@ -17,6 +23,9 @@ postcard_tex = """\\documentclass{article}
     bottom=0.50in]{geometry}
 
 \\begin{document}
+"""
+
+postcard_tex = """
 Hello, World!
 
 This is just a little note to see how you're doing. I haven't see out since that big party in stdlib's house. I hope you've gotten out of that infinite loop I wrote after my 5th beer.
@@ -38,25 +47,73 @@ Big City, ES 12345
 123 Knuth Dr \\\\
 Central Processor, UT 0x000}
 \\vfill
-\\end{document}"""
+"""
 
-def write_latex(file_name):
-    """
-    :param file_name: to which to write the LaTeX
+
+postcard_tex_end = """
+\\end{document}
+"""
+
+
+def write_latex_preamble(out_file):
+    """ Write out the preamble, or start, of the LaTeX document that will contain the postcards.
+
+    :param out_file: is the LaTeX that will contain the postcards
     :return: None
     """
-    with Path(file_name).open('w') as latex_file:
-        latex_file.write(postcard_tex)
+    out_file.write(postcard_tex_preamble)
+
+def write_violation(out_file):
+    """ This emits a postcard for the given violation
+
+    :param out_file:
+    :return:
+    """
+
+def write_latex_end(out_file):
+    """ Write out the end of the LaTeX document for the postcards
+
+    :param out_file: you guessed it, the LaTeX file for the postcards
+    :return: None
+    """
+    out_file.write(postcard_tex_end)
+
+
+def process_violation(violation, out_file):
+    """ This will emit a corresponding postcard for the given violation to the LaTeX postcard file
+
+    :param violation: is an OrderedDict containing details for a specific violation
+    :param out_file: is an open connection to the LaTeX file
+    :return: None
+    """
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Generate a postcard LaTeX file')
 
+    parser.add_argument('--in-file','-i', help='CSV file of curbside violations')
     parser.add_argument('--out-file', '-o', help='Where to write the LaTeX')
 
     args = parser.parse_args()
 
-    write_latex(file_name=args.out_file)
+    curbside_violation_path = Path(violations_csv_file)
+
+    if not curbside_violation_path.exists():
+        print(violations_csv_file, 'does not exist ... exiting')
+        sys.exit(NO_CSV_FILE_ERROR)
+
+    # We've got curbside violations, so let's start writing the LaTeX that will contain all the postcards
+
+    with Path(args.out_file).open('w') as latex_postcards_file:
+        write_latex_preamble(latex_postcards_file)
+        write_latex_end(latex_postcards_file)
+
+        with curbside_violation_path.open('r') as curbside_violation:
+            violations_reader = csv.DictReader(curbside_violation)
+
+            for violation in violations_reader:
+                pprint(violation)
+
 
 
 
