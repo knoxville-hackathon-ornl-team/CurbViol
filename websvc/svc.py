93bc5546d916a8f21edfab64007d9e32abe5626b
Alexandre Sorokine <alex |at| sorokine.info>
2019-03-02 20:06:51 -0500

    several service developed

--- websvc/svc.py
+++ websvc/svc.py
@@ -1,6 +1,5 @@
 
-from flask import Flask
-from flask import request
+from flask import Flask, request, send_from_directory
 
 import psycopg2
 import pprint
@@ -9,17 +8,35 @@ pp = pprint.PrettyPrinter(indent=4)
 
 conn = psycopg2.connect("postgresql://docker:docker@localhost:25432/gis")
 conn.set_session(autocommit=True)
-cur = conn.cursor()
+cur1 = conn.cursor()
+cur2 = conn.cursor()
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='')
+
+@app.route('/index.html')
+def root():
+    return send_from_directory('', 'index.html')
+
+@app.route('/style.css')
+def root_css():
+    return send_from_directory('', 'style.css')
 
 @app.route('/submit', methods=['POST'])
 def submit():
-	cur.execute('INSERT INTO violations("DATE", "HOUSE #", "STREET") VALUES (CURRENT_DATE, %s, %s)', (request.args.get('address','unknown'),request.args.get('details')))
-	return "Updated"
+    cur1.execute('INSERT INTO violations("DATE", "HOUSE #", "STREET") VALUES (CURRENT_DATE, %s, %s)', (request.args.get('address','unknown'),request.args.get('details')))
+    return "Updated"
 
 @app.route('/worst', methods=['GET'])
 def worst():
-    cur.execute('SELECT trim("HOUSE #") || \' \' ||  trim("STREET") as address, count(*) as offences from violations group by 1 order by 2 desc limit 10')
-    return pp.pformat(cur.fetchall())
+    cur1.execute('SELECT trim("HOUSE #") || \' \' ||  trim("STREET") as address, count(*) as offences from violations group by 1 order by 2 desc limit 10')
+    return pp.pformat(cur1.fetchall())
+
+@app.route('/geocode', methods=['PATCH'])
+def geocode():
+    cur1.execute('SELECT address FROM violators WHERE NOT gcattempt')
+    for r in cur1:
+        g = geocoder.osm(r[0])
+        if g.ok:
+            cur2.execute("UPDATE violators SET (lat, lon, gcattemp) = (%s, %s, %s, true)", (g.y, g.x, g.postal))
+    return "Geocode complete"
 
